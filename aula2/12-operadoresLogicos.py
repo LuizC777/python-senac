ed3b5b22cd6d925478a5a0f413ad7f231ee45bf5
LUIZ CLARO LIMA <luiz.claro.lima@gmail.com>
2024-09-18 15:32:23 -0300

    operadores logicos e metodos

---
+++ aula2/12-operadoresLogicos.py
@@ -0,0 +1,18 @@
+# nome=True
+# senha=True
+# resultado=nome and senha
+# print(resultado)
+
+# nome='luiz'
+# senha='1234'
+# resultado=nome=='luiz' and senha=='1234'
+# print(resultado)
+
+# nome='luiz'
+# senha='1234'
+# resultado=nome=='luiz' or senha=='1234'
+# print(f'{resultado}')
+
+variavel=True
+result=not(variavel)
+print(result)
