1e6a6e84712baaf9d18b8797e9b1f54002890a5f
LUIZ CLARO LIMA <luiz.claro.lima@gmail.com>
2024-09-23 17:27:15 -0300

    aula 3 e listaS

---
+++ aula3/23-colecoes-listas.py
@@ -0,0 +1,35 @@
+pessoas = ['luiz','raissa','gabriel','alexandre']
+
+# print(len(pessoas)) # qntd de itens
+
+# print(pessoas[0]) # primeiro item
+
+# print(pessoas[-1]) # ultimo item
+
+# print(pessoas[2:]) # itens a partir de 2
+
+# print(pessoas[1:3]) # itens do 1 ate o *2*
+
+# pessoas[3]='icaro' # altera o indice
+
+#####################################################################################
+
+# pessoas.extend(['livia']) # funcao, adiciona arrays no array
+
+# pessoas.append('ana') # adiciona item no array
+
+# pessoas.insert(3, 'joyce') # adiciona item em um indice declarado
+
+# pessoas.pop(2) # exclui o ultimo quando vazio, ou, um indice
+
+# pessoas.remove('alexandre') # exclui item pelo valor
+
+# pessoas.clear() # exclui todos is itens
+
+# print(pessoas.index('raissa')) # exibe o indice do valor (no primeiro registro)
+
+# print(pessoas.count('raissa')) # conta quantas vezes o valor e citado
+
+# pessoas.sort()
+
+print(pessoas)
