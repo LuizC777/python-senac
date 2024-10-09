3e7750a0233b4776e4ff16432b4abd96b476a549
luiz <luiz.claro.lima@gmail.com>
2024-10-09 00:46:12 -0300

    feito sistema de preco

--- aula7/35-frente-de-caixa.py
+++ aula7/35-frente-de-caixa.py
@@ -1,23 +1,3 @@
-precos={
-    'sanduiches':{
-        'Big Mac' : 20.00,
-        'Duplo Quarteirão' : 25.00,
-        'Big Tasty' : 22.00
-    },
-    'bebidas':{
-        'Coca-Cola' : 5.00,
-        'Fanta' : 5.00,
-        'Del Valle Laranja' : 6.00
-    },
-    'batatas':{
-        'Pequena' : 7.00,
-        'Média' : 9.00,
-        'Grande' : 11.00
-    }
-}
-tipo_batata=''
-tipo_bebida=''
-tipo_sanduiche=''
 # Função que recebe o nome do cliente e inicia o pedido
 
 def pegar_nome_cliente(nome):
@@ -26,17 +6,17 @@ def pegar_nome_cliente(nome):
 # Função que exibe o sanduiche escolhido
 
 def pegar_sanduiche(tipo_sanduiche):
-    print(f'Você escolheu o sanduiche: {tipo_sanduiche}')
+    print(f'Você escolheu o sanduiche: {tipo_sanduiche[0]} R$ {str(tipo_sanduiche[1]).replace('.',',')}')
 
 # Função que exibe a bebida escolhida
 
 def pegar_bebida(tipo_bebida):
-    print(f'Você escolheu a bebida: {tipo_bebida}')
+    print(f'Você escolheu a bebida: {tipo_bebida[0]} R$ {str(tipo_bebida[1]).replace('.',',')}')
 
 # Função que exibe a batata escolhida
-
+batata=''
 def pegar_batata(tipo_batata):
-    print(f'Você escolheu a batata do tamanho: {tipo_batata}')
+    print(f'Você escolheu a batata do tamanho: {tipo_batata[0]} R$ {str(tipo_batata[1]).replace('.',',')}')
 
 # Função que monta o combo e chama as funções necessárias para exibir as escolhas
 
@@ -46,67 +26,98 @@ def montar_combo(nome, tipo_sanduiche, tipo_bebida, tamanho_batata):
     pegar_bebida(tipo_bebida)
     pegar_batata(tamanho_batata)
 
+
 ############################################ Funcao de achar o pedido ############################################
 
+
 def escolher_opcao_sanduiche():
+    sanduiche_custo=0
     while True:
         sanduiche = input('Digite o código do sanduíche: \n1 - Big Mac (R$ 20,00) \n2 - Duplo Quarteirão (R$ 25,00) \n3 - Big Tasty (R$ 22,00) \nV - Voltar \nS - Sair\nOpcao: ')
-        sanduiche.upper()
+        sanduiche=sanduiche.upper()
 
         if sanduiche == "S":
             exit()
+        if sanduiche =='V':
+            sanduiche=4
+        sanduiche=int(sanduiche)
 
-        opcoes = {'1': 'Big Mac', '2' : 'Duplo Quarteirão', '3': 'Big Tasty', 'v':'' }
+        opcoes = {1: 'Big Mac', 2 : 'Duplo Quarteirão', 3: 'Big Tasty', 4:0 }
+        preco_sanduiche = {1: 20.0, 2 : 25.0, 3: 22.0, 4:0}
 
+        if sanduiche in preco_sanduiche:
+            sanduiche_custo=preco_sanduiche[sanduiche]
         if sanduiche in opcoes:
-            return opcoes[sanduiche]
+            return opcoes[sanduiche],sanduiche_custo
         
         print('Opção inválida, tente novamente')
 
+
 def escolher_opcao_bebida():
+    bebida_custo=0
     while True:
         bebida = input('Digite o código da bebida: \n1 - Coca-Cola (R$ 5,00) \n2 - Fanta (R$ 5,00) \n3 - Del Valle Laranja (R$ 6,00) \nV - Voltar \nS - Sair\nOpcao: ')
-        bebida.upper()
+        bebida=bebida.upper()
 
         if bebida == "S":
             exit()
+        if bebida == 'V':
+            bebida=4
+        bebida=int(bebida)
+        
+        opcoes = {1: 'Coca-Cola', 2 : 'Fanta', 3: 'Del Valle Laranja', 4:0 }
+        preco_bebida = {1: 5.0, 2 : 5.0, 3: 6.0, 4:0}
 
-        opcoes = {'1': 'Coca-Cola', '2' : 'Fanta', '3': 'Del Valle Laranja', 'v':'' }
-
+        if bebida in preco_bebida:
+            bebida_custo=preco_bebida[bebida]
         if bebida in opcoes:
-            return opcoes[bebida]
+            return opcoes[bebida],bebida_custo
         
         print('Opção inválida, tente novamente') 
 
+
 def escolher_opcao_batata():
+    batata_custo=0
     while True:
-        batata = input('Digite o código da batata: \n1 - Pequena (R$ 7,00) \n2 - Media (R$ 9,00) \n3 - Grande (R$ 11,00) \nV - Voltar \nS - Sair\nOpcao: ')
-        batata.upper()
+        batata = input('Digite o código da batata: \n1 - Pequena (R$ 7,00) \n2 - Media (R$ 9,00) \n3 - Grande (R$ 11,00) \nv - Voltar \nS - Sair\nOpcao: ')
+        batata=batata.upper()
 
         if batata == "S":
             exit()
+        if batata == 'V':
+            batata=4
+        batata=int(batata)
 
-        opcoes = {'1': 'Pequena', '2' : 'Media', '3': 'Grande', 'v':'' }
+        opcoes = {1: 'Pequena', 2 : 'Media', 3: 'Grande', 4:0 }
+        preco_batata = {1 : 7.0, 2 : 9.0, 3 : 11.0, 4:0 }
 
+        if batata in preco_batata:
+            batata_custo=preco_batata[batata]
         if batata in opcoes:
-            return opcoes[batata]
-        
+            print(batata_custo)
+            return opcoes[batata],batata_custo
         print('Opção inválida, tente novamente')
+
 
 def fim():
 
-
     print(f'{''.center(10,'*')}\nPedido:\n{nome_cliente}')
-    if tipo_sanduiche:
+    if tipo_sanduiche!=(0,0):
         pegar_sanduiche(tipo_sanduiche)
-    if tipo_bebida:
+    if tipo_bebida!=(0,0):
         pegar_bebida(tipo_bebida)
-    if tipo_batata:
+    if tipo_batata!=(0,0):
         pegar_batata(tipo_batata)
+    print(f'Total: R$ {str((tipo_batata[1])+(tipo_bebida[1])+(tipo_sanduiche[1])).replace('.',',')}')
     print(f'\n{''.center(10,'*')}')
     
 
+tipo_batata=(0,0)
+tipo_bebida=(0,0)
+tipo_sanduiche=(0,0)
+
 ############################################ inicio do pedido ############################################
+
 
 while True:
     nome_cliente = input('Digite o nome do cliente (ou [s] para sair): ')
